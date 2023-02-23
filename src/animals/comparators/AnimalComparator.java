a75363f854ae9eee0a6b8a80faae902259652679
Kate Belozertseva <katebelozertseva@gmail.com>
2023-02-23 23:40:35 +0300

    Corrected WolfComparator with Integer.compare

--- src/animals/comparators/AnimalComparator.java
+++ src/animals/comparators/AnimalComparator.java
@@ -1,2 +1,12 @@
-package animals.comparators;public class AnimalComparator {
+package animals.comparators;
+
+import animals.Animal;
+
+import java.util.Comparator;
+
+public class AnimalComparator implements Comparator<Animal> {
+    @Override
+    public int compare(Animal o1, Animal o2) {
+        return Integer.compare(o1.getAge(), o2.getAge());
+    }
 }
